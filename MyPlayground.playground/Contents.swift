59ca520cd34ece4553a6ef0f0189780a604001cc
Movses <agabekyanmovses@gmail.com>
2021-10-01 18:05:13 +0400

    oct1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,58 +1,90 @@
 import Cocoa
 
-class Person {
-    var name = ""
-    var age = 0
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
-    }
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    func isToTheRightOf(x: Double) -> Bool {
+//        return self.x > x
+//    }
+//}
+//
+//let some = Point(x: 3.0, y: 4.0)
+//print(some.isToTheRightOf(x: 2.0))
 
-   
+
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+//        x += deltaX
+//        y += deltaY
+//    }
+//}
+//var somePoint = Point(x: 1.0, y: 1.0)
+//somePoint.moveBy(x: 2.0, y: 3.0)
+//print("The point is now at (\(somePoint.x), \(somePoint.y))")
+
+
+
+
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+//        self = Point(x: x + deltaX, y: y + deltaY)
+//    }
+//}
+//
+//
+//var some = Point(x: 5.0, y: 4.0)
+//some.moveBy(x: 5.0, y: 6.0)
+//print(some)
+
+
+
+//enum TriStateSwitch {
+//    case off, low, high
+//    mutating func next() {
+//        switch self {
+//        case .off:
+//            self = .low
+//        case .low:
+//            self = .high
+//        case .high:
+//            self = .off
+//        }
+//    }
+//}
+
+
+
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    mutating func moveBy(a deltaX: Double, b deltaY: Double)  {
+//        x += deltaX
+//        y += deltaY
+//    }
+//}
+//
+//var point = Point(x: 1.0, y: 2.0)
+//point.moveBy(a: 2.0, b: 4.0)
+//
+//print(point)
+
+
+enum Switch {
+    case off
+    case low
+    case high
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
 }
 
-class Employee: Person {
-    
-    var salary = 0
-    
-    func doWork() {
-        print("Hi my name is \(name) and I have \(salary) salary")
-    }
-}
-
-
-class Manager: Employee {
-    
-    var teamSize = 0
-
-    override func doWork() {
-        
-        super.doWork()
-        
-        salary += 5000
-        print("I am managing people \(name) ")
-
-
-    }
-    
-    func hiringPeople() {
-        print("I can hire people ")
-    }
-    
-}
-
-
-let p = Person(name: "jbjb")
-let e = Employee()
-let m = Manager(n)
-print (p.name)
-print (e.name)
-print (m.name)
-
-p.age = 32
-m.age = 343
-print(m.age)
-
-
-
+var sw = Switch.off
+sw.next()
+print(sw)
---
+++ taskForOct1/taskForOct1/main.swift
@@ -0,0 +1,113 @@
+//
+//  main.swift
+//  taskForOct1
+//
+//  Created by Movses Aghabekyan on 30.09.21.
+//
+
+import Foundation
+
+// Create a struct called Student which has 2 properties student name and grade. Create a class which has an array of Students and has method to add new student to this array, method to remove all students and method to print current list of students.
+
+
+struct Student {
+    var name: String
+    var grade: Int
+}
+
+class Students {
+    var arrOfStudents = [String]()
+    
+    func addNewStudent(student: String) -> [String] {
+        arrOfStudents.append(student)
+        return arrOfStudents
+    }
+    
+    func removeStudents() -> [String] {
+        arrOfStudents.removeAll()
+        return arrOfStudents
+    }
+    
+    func printStudents(students: [String]) -> [String] {
+        return arrOfStudents
+    }
+}
+var person = Student(name: "Mrdo", grade: 12)
+let someClass = Students()
+print(someClass.addNewStudent(student: person.name))
+
+
+
+// Create a class which has one Int property and one method which increment that property value. Experiment with self when accessing property.
+
+class SimpleClass {
+    var num = 99
+   
+    
+    func incrementor(num: Int) -> Int {
+        
+        return self.num + num
+    }
+}
+
+var inc = SimpleClass()
+print(inc.incrementor(num: 5))
+
+
+
+// Create a struct which has 2 Int properties. Experiment with mutating keyword for struct's method which modifies that properties.
+
+struct Mutant {
+    var num1: Int, num2: Int
+  
+    mutating func increment() {
+        num1 += 1
+        num2 += 2
+    }
+    
+}
+
+
+var mutate = Mutant(num1: 4, num2: 5)
+mutate.increment()
+print(mutate)
+
+
+// Create a class which has 2 Int properties. One of the properties should be static. Also create 2 methods and make one of them static. Experiment with static method non static property and static property non static method usage. Also experiment with static method and static property.
+
+
+
+class RichestMan {
+    static var money = 1000
+    static var name = "Jane"
+    var regularPersonMoney = 0
+    var regularPersonName = "" {
+        didSet {
+            checkRichestMan()
+        }
+    }
+    
+    init(regularPersonMoney: Int, regularPersonName: String) {
+        self.regularPersonMoney = regularPersonMoney
+        self.regularPersonName = regularPersonName
+    }
+    
+    func checkRichestMan() {
+        if regularPersonMoney > RichestMan.money {
+            RichestMan.money = regularPersonMoney
+            RichestMan.name = regularPersonName
+            
+
+        }
+        
+    }
+        static func printRichestMan() -> (){
+            print("The richest man \(name) has \(money)$$")
+    }
+    
+}
+
+
+var millioner = RichestMan(regularPersonMoney: 3222, regularPersonName: "Anna")
+print(RichestMan.printRichestMan())
+
