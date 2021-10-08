abc1cd5ec514a0ea21b610138905fb7343b13476
Movses <agabekyanmovses@gmail.com>
2021-10-08 22:19:19 +0400

    Updated

--- Protocols/Protocols/main.swift
+++ Protocols/Protocols/main.swift
@@ -16,7 +16,7 @@ protocol PersonInterface {
     var lastName: String { get  }
     
     func sayYourName()
-    
+    init(firstName: String, lastName: String)
 }
 
 
@@ -29,7 +29,7 @@ class Person: PersonInterface {
         print("\(firstName) \(lastName)")
     }
     
-    init(firstName: String, lastName: String) {
+    required init(firstName: String, lastName: String) {
         self.firstName = firstName
         self.lastName = lastName
     }
@@ -43,14 +43,6 @@ person.sayYourName()
 
 
 // Modify previous example and add an init method inside PersonInterface protocol. That init should get 2 string parameters for first and last names.
-
-
-extension PersonInterface {
-    init(firstName: String, lastName: String) {
-        self.init(firstName: firstName, lastName: lastName)
-    }
-    
-}
 
 
 // Create FootballPlayer protocol and inherit it from PersonInterface protocol. Add clubName function to that protocol which is not getting or returning any parameter. Modify previous example so Person class will conform to FootballPlayer protocol. As an implementation for clubName just randomly print some football club name. Create instance of the Person class and call all methods defined in the protocol.
@@ -96,34 +88,29 @@ class Human: OnlyForClass {
 
 // Implement ClassRoom class which has addStudent method and Int property for current student count. Define protocol named ClassRoomDelegate and declare one method named studentAdded which has only 1 Int parameter named currentStudentCount. Define School class which conforms to ClassRoomDelegate protocol via extension. Inside School class add property named classroom which is of type ClassRoom. Add weak property for ClassRoom named delegate. For ClassRoom class addStudent method add the following implementation. When addStudent method called currentStudentCount should be incremented and you should call studentAdded method via delegate to distribute the current number of students. School implementation for studentAdded should print the number of students in the classroom. Create object of type School and then call addStudent method several times to see how you correctly print the number of students in the classroom.
 
-
-class ClassRoom {
-    func addStudent() {
-        currentStudentsCount += 1
-        delegate?.studentAdded(currentStudentCount: 0)
-    }
-    
-    var currentStudentsCount = 0
-    
-    
-    
-    weak var delegate: ClassRoomDelegate?
-}
-
-
 protocol ClassRoomDelegate: AnyObject {
     func studentAdded(currentStudentCount: Int)
+}
+
+class ClassRoom {
     
+    var currentStudentsCount = 0
+
+    weak var delegate: ClassRoomDelegate?
+    
+    func addStudent() {
+        currentStudentsCount += 1
+        delegate?.studentAdded(currentStudentCount: currentStudentsCount)
+    }
 }
 
 class School {
     
-    var classRoom: ClassRoom?
+    var classRoom: ClassRoom
     
     init(classRoom: ClassRoom) {
         self.classRoom = classRoom
     }
-    
     
 }
 
@@ -135,5 +122,17 @@ extension School: ClassRoomDelegate {
 }
 
 
-var school = School()
+var classRoom = ClassRoom()
+var school = School(classRoom: classRoom)
+
+classRoom.addStudent()
+classRoom.addStudent()
+classRoom.addStudent()
+classRoom.addStudent()
+
+classRoom.delegate = school
+
+classRoom.addStudent()
+
+
 
