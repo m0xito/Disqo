1fe5e15fe54fe4915a497369e0b1fec3fdd4e9f7
Movses <agabekyanmovses@gmail.com>
2021-10-11 17:50:46 +0400

    Oct11

--- Generics/Generics/main.swift
+++ Generics/Generics/main.swift
@@ -35,14 +35,22 @@ enum MoreOrLess {
 }
 
 
-compare(a: 9, b: 9)
+print(compare(a: 9, b: 9))
 
 
 
 // Extend previous example and create a class named Person which has age and weight properties. Define <, > and equal operators. Create 2 instances of Person class and call compare method and print results.
 
 
-class Person {
+class Person: Comparable {
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return lhs.age == rhs.age && lhs.weight == rhs.weight
+    }
+    
+    static func < (lhs: Person, rhs: Person) -> Bool {
+        return lhs.age < rhs.age && lhs.weight < rhs.weight
+    }
+    
     
     
     let age: Int
@@ -64,21 +72,21 @@ compare(a: personOne.weight, b: personTwo.weight)
 
 // Extend pervious example to add type constraint syntax. After your changes compare method should work only with class types and does not work for Int, String, Double etc.
 
-protocol CompareClassProtocol: AnyObject {
+func compareClasses<T: Comparable>(a: T, b: T) -> MoreOrLess where T: AnyObject   {
     
-    func compareProtocols<T: Comparable>(class1: T, class2: T)
+    
+    if a > b {
+        return MoreOrLess.greater
+    } else if a == b {
+        return MoreOrLess.equal
+    } else {
+        return MoreOrLess.less
+    }
     
     
 }
 
-
-
-
-
-
-
-
-
+print(compareClasses(a: personOne, b: personTwo))
 
 
 // Define a generic class named Multiplicator. Declare 2 properties inside it and init them via initializer. Define multiply method which should return the result of multiplication of the defined 2 properties.
