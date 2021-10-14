c529c72ca3bf55600fdb347ebb492d10a7c4ba58
Movses <agabekyanmovses@gmail.com>
2021-10-14 18:37:54 +0400

    Updated
    In the process

--- Deque/Deque/main.swift
+++ Deque/Deque/main.swift
@@ -1,113 +1,56 @@
-class MyCircleQueue<T> {
+class Queue<T> {
     
-    var arr: [T] = []
-    var counter = 0
+    private var arr: [T?]
+    var isEmpty: Bool {
+        return arr.isEmpty
+    }
+    private var size: Int {
+        return arr.count
+    }
+    init(arr: [T?]) {
+        self.arr = []
+    }
     
     init(_ k: Int) {
-        arr = Array(repeating: -1 as! T, count: k)
+        arr = Array(repeating: nil, count: k)
     }
     
-    func pushFront(_ value: T) -> Bool {
-        if isFull() {
-            return false
-        } else {
-            arr.removeLast()
-            arr[0] = value
-            counter += 1
-            return true
-        }
+    
+    init(deque: [T]) {
+        arr = deque
         
     }
     
-    func popFront(_ value: T) -> Bool {
-        if isEmpty() {
-            return false
-        } else {
-            arr[counter - 1] = -1 as! T
-            counter -= 1
-            return true
-        }
-            
-    }
-    
-    func first() -> Int {
-        if isEmpty() {
-            return -1
-        } else {
-            return arr[counter] as! Int
+    func popBack() {
+        if isEmpty == false {
+        arr.removeLast()
         }
     }
     
-    func last() -> Int {
-        if isEmpty() {
-            return -1
-        } else {
-            return arr[0] as! Int
+    func popFront() {
+        if isEmpty == false {
+            arr.removeFirst()
         }
     }
     
-    func isEmpty() -> Bool {
-        if (counter > 0) ||  (leftStack.isEmpty && rightStack.isEmpty) {
-            return false
-        } else {
-            return true
+    func pushFirst(element: T) {
+                                   
+    }
+    
+    func first() -> T? {
+        if arr.isEmpty == false {
+            return arr[0]
         }
+        return nil
     }
     
-    func isFull() -> Bool {
-        if counter == arr.count {
-            return true
-        } else {
-            return false
+    func last() -> T? {
+        if arr.isEmpty == false {
+            return arr[arr.count - 1]
         }
+        return nil
     }
     
-    var leftStack: [T] = []
-    var rightStack: [T] = []
-   
-    var peek: T? {
-        leftStack.isEmpty ? rightStack.first : leftStack.last
-    }
     
-    var description: String {
-        if isEmpty() {
-            return "Queue is empty"
-        }
-        if leftStack.isEmpty == false {
-            arr.append(contentsOf: rightStack)
-        }
-        return "------Queue staeted------\n" +
-        arr.map({"\($0)"}).joined(separator: " --> ") +
-        "\n-----Queue Finished-----"
-    }
-    
-    func pushBack(_ element: T) {
-        rightStack.append(element)
-    }
-    
-    func popBack() -> T? {
-        
-        if isEmpty() {
-            return nil
-        }
-        
-        if leftStack.isEmpty {
-            leftStack = rightStack.reversed()
-            rightStack.removeAll()
-        }
-        
-        return leftStack.removeLast()
-    }
-    
-    func insert(index: Int, element: T) {
-        arr.insert(element, at: index)
-    }
-    
-    func printArr() {
-        print(arr)
-    }
     
 }
-var inst = MyCircleQueue<Any>(10)
-inst.printArr()
-
