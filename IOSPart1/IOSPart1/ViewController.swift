2f787263d37befb2f09c59a5cd63d13396598146
Movses <agabekyanmovses@gmail.com>
2021-11-03 20:05:31 +0400

    Updated

--- IOSPart1/IOSPart1/ViewController.swift
+++ IOSPart1/IOSPart1/ViewController.swift
@@ -9,43 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
-    @IBOutlet weak var number: UILabel!
-    var n = 0
-    
-    @IBAction func pressToSwitch(_ sender: UIButton) {
-        if sender.currentTitle == "Switch On" {
-            sender.setTitle("Switch Off", for: .normal)
-            n = 1
-            sender.setTitleColor(.red, for: .normal)
-            
-        } else {
-            n = 0
-            sender.setTitle("Switch On", for: .normal)
-            sender.setTitleColor(.green, for: .normal)
-        }
-    }
-    
-    @IBAction func genetator(_ sender: UIButton) {
-        let a = Int.random(in: 0...10)
-        let b = Int.random(in: 11...100)
-        
-        if n == 0 {
-            number.text = String(a)
-        } else {
-            number.text = String(b)
-        }
-    }
-    
+    let label = UILabel()
+    let generateButton = UIButton()
+    let switchControllButton = UISwitch()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.addSubview(number)
         
+        
+        // Label
+        
+        view.addSubview(label)
+        label.text = "0"
+        label.textColor = .blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32.0)
+        
+        
+        // Generate Button
+        
+        view.addSubview(generateButton)
+        generateButton.setTitle("GENETATE", for: .normal)
+        generateButton.setTitleColor(.red, for: .normal)
+        generateButton.titleLabel?.font =  UIFont(name: "", size: 40)
+        generateButton.frame = CGRect(x: Int(self.view.bounds.width / 2.0 - 50), y: Int(self.view.bounds.height - 120), width: 100, height: 100)
+        generateButton.addTarget(self, action: #selector(letAction), for: .touchUpInside)
+        
+       
+        
+        // Switch controller
+        
+        view.addSubview(switchControllButton)
+        switchControllButton.frame = CGRect(x: Int(self.view.bounds.width / 2.0 - 25), y: Int(self.view.bounds.height - 250), width: 100, height: 100)
     }
-
-
+    
+    @objc func letAction() {
+        if switchControllButton.isOn {
+            label.text = String(Int.random(in: 0...10))
+        } else {
+            label.text = String(Int.random(in: 11...100))
+        }
+    }
 }
-
